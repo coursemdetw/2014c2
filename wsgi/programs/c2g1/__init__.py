f976b02bfc4108bded7bf63f7d9bc52350d6a8a4
chiaming yen <chiamingyen@gmail.com>
2014-05-04 21:41:06 +0800

    add animate1 and flag to c2g1

--- wsgi/programs/c2g1/__init__.py
+++ wsgi/programs/c2g1/__init__.py
@@ -145,3 +145,146 @@ class C2G1(object):
     </html>
     '''
         return outstring
+        
+    @cherrypy.expose
+    def animate1(self, *args, **kwargs):
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <script type="text/javascript" src="/static/Brython2.1.0-20140419-113919/brython.js"></script>
+    </head>
+    <body onload="brython({debug:1, cache:'version'})">
+    <canvas id="plotarea" width="800" height="800"></canvas>
+    <script type="text/python">
+    # 導入 doc
+    from browser import doc
+    import browser.timer
+    
+    # 準備繪圖畫布
+    canvas = doc["plotarea"]
+    ctx = canvas.getContext("2d")
+    # 進行座標轉換, x 軸不變, y 軸反向且移動 800 光點
+    ctx.setTransform(1, 0, 0, -1, 0, 800)
+    # 設定 y 的起始值
+    y = 0
+    # 設定增量變數 inc 為 1
+    inc = 1
+    # 將畫筆設為紅色
+    ctx.strokeStyle = "rgb(255, 0, 0)"
+    
+    # 定義畫水平線函式
+    def draw():
+        # 將 y 與 inc 設為全域變數
+        global y, inc
+        # 畫新圖之前, 先清除畫面
+        ctx.clearRect(0, 0, 800, 800)
+        # 開始畫水平線
+        ctx.beginPath()
+        ctx.moveTo(0, y)
+        ctx.lineTo(800-1, y)
+        ctx.stroke()
+        # y 的值增量
+        y = y + inc
+        # 若 y 碰到兩個極端值, 則改變增量方向
+        if y == 0 or y == 800-1:
+            inc = inc*-1
+
+    # ev 為事件輸入, 與隨後的 bind 方法配合
+    def start(ev):
+        # interval 為全域變數, 因為 stop 函式需要
+        global interval
+        # 每 10 個 micro second 呼叫一次 draw
+        interval = browser.timer.set_interval(draw,10)
+        
+    def stop(ev):
+        global interval
+        browser.timer.clear_interval(interval)
+
+    # 將 id 為 start 的按鈕與 start  函式利用 click 事件加以連結
+    doc['start'].bind('click', start)
+    # 將 id 為 stop 的按鈕與 stop  函式利用 click 事件加以連結
+    doc['stop'].bind('click', stop)
+    
+    </script>
+    <!-- 這裡建立 start 與 stop 按鈕-->
+    <button id="start">start</button>
+    <button id="stop">stop</button>
+    </body>
+    </html>
+    '''
+        return outstring
+        
+    @cherrypy.expose
+    def flag(self, *args, **kwargs):
+        '''
+        原始程式來源: http://blog.roodo.com/esabear/archives/19215194.html
+        改寫為 Brython 程式
+        '''
+        outstring = '''
+    <!DOCTYPE html> 
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <script type="text/javascript" src="/static/Brython2.1.0-20140419-113919/brython.js"></script>
+    </head>
+    <body onload="brython({debug:1, cache:'version'})">
+    <canvas id="plotarea" width="300" height="200"></canvas>
+    <script type="text/python">
+    # 導入 doc
+    from browser import doc
+    import math
+
+    # 準備繪圖畫布
+    canvas = doc["plotarea"]
+    ctx = canvas.getContext("2d")
+    # 進行座標轉換, x 軸不變, y 軸反向且移動 canvas.height 單位光點
+    # ctx.setTransform(1, 0, 0, -1, 0, canvas.height)
+    # 以下採用 canvas 原始座標繪圖
+    flag_w = canvas.width
+    flag_h = canvas.height
+    circle_x = flag_w/4
+    circle_y = flag_h/4
+    # 先畫滿地紅
+    ctx.fillStyle='rgb(255, 0, 0)'
+    ctx.fillRect(0,0,flag_w,flag_h)
+    # 再畫青天
+    ctx.fillStyle='rgb(0, 0, 150)'
+    ctx.fillRect(0,0,flag_w/2,flag_h/2)
+    # 畫十二到光芒白日
+    ctx.beginPath()
+    star_radius = flag_w/8
+    angle = 0
+    for i in range(24):
+        angle += 5*math.pi*2/12
+        toX = circle_x + math.cos(angle)*star_radius
+        toY = circle_y + math.sin(angle)*star_radius
+        # 只有 i 為 0 時移動到 toX, toY, 其餘都進行 lineTo
+        if (i):
+            ctx.lineTo(toX, toY)
+        else:
+            ctx.moveTo(toX, toY)
+    # 將填色設為白色
+    ctx.fillStyle = '#fff'
+    ctx.fill()
+    # 白日:藍圈
+    ctx.beginPath()
+    # 查詢 canvas arc 如何定義
+    ctx.arc(circle_x, circle_y, flag_w*17/240, 0, math.pi*2, true)
+    ctx.closePath()
+    # 填色設為藍色
+    ctx.fillStyle = 'rgb(0, 0, 149)'
+    ctx.fill()
+    # 白日:白心
+    ctx.beginPath()
+    ctx.arc(circle_x, circle_y, flag_w/16, 0, math.pi*2, true)
+    ctx.closePath()
+    # 填色設為白色
+    ctx.fillStyle = '#fff'
+    ctx.fill()
+    </script>
+    </body>
+    </html>
+    '''
+        return outstring
